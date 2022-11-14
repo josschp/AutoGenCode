8c45d45ba63be32acfec539dc576600a4f65fe02
josschp <102892656+josschp@users.noreply.github.com>
2022-11-15 00:18:19 +0800

    Add Connection to the Server

--- AutoGenCode/Connection.cs
+++ AutoGenCode/Connection.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace AutoGenCode
 {
     internal class Connection
     {
+        private static MySqlConnection mconn =new MySqlConnection();
+        public Connection()
+        {
+            mconn.ConnectionString = $"server=localhost;uid=root;pwd=;";
+        }
+
+        public Connection(string sv, string uid, string psw)
+        {
+            mconn.ConnectionString=$"server = {sv}; uid = {uid}; pwd = {psw};database = information_schema";
+        }
+
+        public Connection(string sv, string uid, string psw, string dataname)
+        {
+            mconn.ConnectionString = $"server = {sv}; uid = {uid}; pwd = {psw};database = {dataname}";
+        }
+
+        public DataTable SchemasLoad()
+        {
+            if (mconn.State == ConnectionState.Closed)
+                mconn.Open();
+            DataTable dtTable = new DataTable();
+            string sql = "select table_schema as database_name, table_name from information_schema.tables where table_type = 'BASE TABLE' group by database_name order by database_name, table_name;";
+            var comm = new MySqlCommand(sql, mconn);
+            MySqlDataAdapter ad = new MySqlDataAdapter(comm);
+            ad.Fill(dtTable);
+            mconn.Close();
+            return dtTable;
+        }
+        ~Connection() { }
     }
 }
--- AutoGenCode/Form1.cs
+++ AutoGenCode/Form1.cs
@@ -1,15 +1,42 @@
+using System.Data;
+
 namespace AutoGenCode
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
+
             InitializeComponent();
         }
 
+        private static string server;
+        private static string user;
+        private static string password;
+        private Connection myConnect;
+
         private void btnConnect_Click(object sender, EventArgs e)
         {
-
+            server = txtIPAdd.Text;
+            user = txtUsername.Text;
+            password = txtPass.Text;
+            myConnect = new Connection(server, user, password);
+            try
+            {
+                DataTable schemas = myConnect.SchemasLoad();
+                if (schemas.Rows.Count > 0)
+                {
+                    for (int i = 0; i < schemas.Rows.Count; i++)
+                    {
+                        cmbSchemas.Items.Add(schemas.Rows[i][0].ToString());
+                    }
+                    cmbSchemas.SelectedIndex = 0;
+                }
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void cmbSchemas_SelectedIndexChanged(object sender, EventArgs e)
@@ -26,5 +53,7 @@ namespace AutoGenCode
         {
 
         }
+
+        ~Form1() { }
     }
 }
